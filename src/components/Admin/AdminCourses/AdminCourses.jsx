9b812ff9adebb0449ad787105c807a5f66e5b6ec
Ritik Kumar Pathak <ritik.p@awesomesuite.com>
2024-01-28 21:49:43 +0530

    frontend-completed

--- src/components/Admin/AdminCourses/AdminCourses.jsx
+++ src/components/Admin/AdminCourses/AdminCourses.jsx
@@ -1,25 +1,181 @@
 import React from 'react';
 import MetaData from '../../../Meta/MetaData';
-import { Box, Grid } from '@chakra-ui/react';
+import {
+  Box,
+  Button,
+  Grid,
+  HStack,
+  Heading,
+  Image,
+  Table,
+  TableCaption,
+  TableContainer,
+  Tbody,
+  Td,
+  Th,
+  Thead,
+  Tr,
+  useDisclosure,
+} from '@chakra-ui/react';
 import Cursor from '../../../assets/images/cursor.png';
 import Sidebar from '../Dashboard/Sidebar';
+import CourseModal from './CourseModal';
 
 const AdminCourses = () => {
+  const courses = [
+    {
+      _id: 1,
+      poster: {
+        url: <Cursor />,
+      },
+      title: 'React Course',
+      category: 'development',
+      createdBy: 'Ritik Pathak',
+      views: 300,
+      numOfVideos: 15,
+    },
+    {
+      _id: 2,
+      poster: {
+        url: <Cursor />,
+      },
+      title: 'React Course',
+      category: 'development',
+      createdBy: 'Ritik Pathak',
+      views: 300,
+      numOfVideos: 15,
+    },
+    {
+      _id: 3,
+      poster: {
+        url: <Cursor />,
+      },
+      title: 'React Course',
+      category: 'development',
+      createdBy: 'Ritik Pathak',
+      views: 300,
+      numOfVideos: 15,
+    },
+  ];
+
+  const { isOpen, onClose, onOpen } = useDisclosure();
+
+  const courseDetailHandler = id => {
+    // console.log('updated', id);
+    onOpen();
+  };
+
+  const deleteLectureHandler = id => {
+    console.log('deleted', id);
+  };
+
+  const deleteButtonHandler = (courseId, lectureId) => {
+    console.log('CourseId', courseId);
+    console.log('LectureId', lectureId);
+  };
+
+  const addLectureHandler = (e, courseId, title, description, video) => {
+    e.preventDefault();
+    console.log('Form Submitted');
+  };
+
   return (
     <>
-      <MetaData title={'Learn Hub || Admin All Courses'} />
+      <MetaData title={'Learn Hub || Admin All Users'} />
       <Grid
         minH={'100vh'}
         templateColumns={['1fr', '5fr 1fr']}
         css={{ cursor: `url(${Cursor}),default` }}
       >
-        <Box>
-
+        <Box p={['0', '8']} overflowX={'auto'}>
+          <Heading
+            textTransform={'uppercase'}
+            children="All Lectures"
+            m={16}
+            textAlign={['center', 'left']}
+          />
+          <TableContainer w_idth={['100vw', 'full']}>
+            <Table variant={'stripe'} size={'lg'}>
+              <TableCaption children="All Available Lectures" />
+              <Thead>
+                <Tr>
+                  <Th>Id</Th>
+                  <Th>Poster</Th>
+                  <Th>Title</Th>
+                  <Th>Category</Th>
+                  <Th>Creator</Th>
+                  <Th isNumeric justifyContent={'flex-end'}>
+                    Views
+                  </Th>
+                  <Th isNumeric justifyContent={'flex-end'}>
+                    Lecture
+                  </Th>
+                  <Th isNumeric justifyContent={'flex-end'}>
+                    Action
+                  </Th>
+                </Tr>
+              </Thead>
+              <Tbody>
+                {courses.map(item => (
+                  <Row
+                    courseDetailHandler={courseDetailHandler}
+                    deleteLectureHandler={deleteLectureHandler}
+                    key={item._id}
+                    item={item}
+                  />
+                ))}
+              </Tbody>
+              <Tbody></Tbody>
+            </Table>
+          </TableContainer>
+          <CourseModal
+            // lectures
+            isOpen={isOpen}
+            onClose={onClose}
+            onOpen={onOpen}
+            id={'anjsjjs'}
+            deleteButtonHandler={deleteButtonHandler}
+            addLectureHandler={addLectureHandler}
+            courseTitle={'React Js'}
+          />
         </Box>
-        <Sidebar/>
+        <Sidebar />
       </Grid>
     </>
   );
 };
 
 export default AdminCourses;
+
+function Row({ item, courseDetailHandler, deleteLectureHandler }) {
+  return (
+    <Tr>
+      <Td>{item._id}</Td>
+      <Td>
+        <Image src={item.poster.url} />
+      </Td>
+      <Td>{item.title}</Td>
+      <Td textTransform={'uppercase'}>{item.category}</Td>
+      <Td>{item.createdBy}</Td>
+      <Td isNumeric>{item.views}</Td>
+      <Td isNumeric>{item.numOfVideos}</Td>
+      <Td isNumeric>
+        <HStack justifyContent={'flex-end'} ml={-12}>
+          <Button
+            variant={'outlined'}
+            color={'purple'}
+            onClick={() => courseDetailHandler(item._id)}
+          >
+            View Lecture
+          </Button>
+          <Button
+            colorScheme="red"
+            onClick={() => deleteLectureHandler(item._id)}
+          >
+            Delete
+          </Button>
+        </HStack>
+      </Td>
+    </Tr>
+  );
+}
---
+++ src/components/Admin/AdminCourses/CourseModal.jsx
@@ -0,0 +1,168 @@
+import {
+  Box,
+  Button,
+  Grid,
+  Heading,
+  Input,
+  Modal,
+  ModalBody,
+  ModalCloseButton,
+  ModalContent,
+  ModalFooter,
+  ModalHeader,
+  ModalOverlay,
+  Stack,
+  Text,
+  VStack,
+} from '@chakra-ui/react';
+import React, { useState } from 'react';
+import { RiDeleteBack2Fill } from 'react-icons/ri';
+import { fileUploadCss } from '../../Auth/Register/Register';
+
+const CourseModal = ({
+  lectures = [1, 2, 3, 4, 5, 6, 7, 8, 9],
+  isOpen,
+  onClose,
+  id,
+  deleteButtonHandler,
+  courseTitle,
+  addLectureHandler,
+}) => {
+  const [title, setTitle] = useState('');
+  const [description, setDescription] = useState('');
+  const [video, setVideo] = useState('');
+  const [videoPrev, setVideoPrev] = useState('');
+
+  const changeVideoHandler = event => {
+    const file = event.target.files[0];
+    const reader = new FileReader();
+    reader.readAsDataURL(file);
+    reader.onload = () => {
+      setVideoPrev(reader.result);
+      setVideo(file);
+    };
+  };
+
+  return (
+    <>
+      <Modal
+        isOpen={isOpen}
+        onClose={onClose}
+        size={'full'}
+        scrollBehavior="outside"
+      >
+        <ModalOverlay />
+        <ModalContent>
+          <ModalHeader>{courseTitle}</ModalHeader>
+          <ModalCloseButton />
+          <ModalBody p={16}>
+            <Grid templateColumns={['1fr', '3fr 1fr']}>
+              <Box px={[0, 16]}>
+                <Box my={5}>
+                  <Heading children={courseTitle} />
+                  <Heading children={`#${id}`} size={'sm'} opacity={0.4} />
+                  <Heading children={'Lectures'} size={'lg'} />
+                  {lectures.map((item, index) => (
+                    <VideoCard
+                      key={index}
+                      title={'ReactJs'}
+                      description={'nsdkndsdjsfdjsjfdkifdjdeffjfjef'}
+                      num={index + 1}
+                      lectureId={'ghdhd'}
+                      courseId={id}
+                      deleteButtonHandler={deleteButtonHandler}
+                    />
+                  ))}
+                </Box>
+              </Box>
+              <Box>
+                <form
+                  onSubmit={e =>
+                    addLectureHandler(e, id, title, description, video)
+                  }
+                >
+                  <VStack spacing={4}>
+                    <Heading
+                      children="Add Lecture"
+                      size={'md'}
+                      textTransform={'uppercase'}
+                    />
+                    <Input
+                      focusBorderColor="purple.400"
+                      placeHolder="Enter The Title"
+                      value={title}
+                      onChange={e => setTitle(e.target.value)}
+                    />
+                    <Input
+                      focusBorderColor="purple.400"
+                      placeHolder="Enter The Description"
+                      value={description}
+                      onChange={e => setDescription(e.target.value)}
+                    />
+                    <Input
+                      accept="video/*"
+                      required
+                      type="file"
+                      focusBorderColor="purple.500"
+                      css={{ '&::file-selector-button': fileUploadCss }}
+                      onChange={changeVideoHandler}
+                    />
+                    {videoPrev && (
+                      <video
+                        controlsList="nodownload"
+                        controls
+                        src={videoPrev}
+                      ></video>
+                    )}
+                    <Button width={'full'} colorScheme="purple" type="submit">
+                      Add Lecture
+                    </Button>
+                  </VStack>
+                </form>
+              </Box>
+            </Grid>
+          </ModalBody>
+          <ModalFooter>
+            <Button onClick={onClose}>Close</Button>
+          </ModalFooter>
+        </ModalContent>
+      </Modal>
+    </>
+  );
+};
+
+export default CourseModal;
+
+// Function For Video Card
+function VideoCard({
+  title,
+  description,
+  num,
+  lectureId,
+  courseId,
+  deleteButtonHandler,
+}) {
+  return (
+    <>
+      <Stack
+        direction={['column', 'row']}
+        my={8}
+        borderRadius={'lg'}
+        boxShadow={'0 0 10px rgba(0,0,0,0.6)'}
+        justifyContent={['flex-start', 'space-between']}
+        p={[4, 8]}
+      >
+        <Box>
+          <Heading size={'sm'} children={`#${num} ${title}`} />
+          <Text children={description} />
+        </Box>
+        <Button
+          color={'purple.600'}
+          onClick={() => deleteButtonHandler(courseId, lectureId)}
+        >
+          <RiDeleteBack2Fill />
+        </Button>
+      </Stack>
+    </>
+  );
+}
